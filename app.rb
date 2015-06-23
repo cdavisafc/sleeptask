a1330531b934e753fd90171f05885e89a7ac1f74
Cornelia Davis <cornelia@corneliadavis.com>
2015-06-23 08:59:02 -0700

    simplify

--- app.rb
+++ app.rb
@@ -1,11 +1,6 @@
-module App
-  class Main 
-    numSecs = 5 #default
-    if ARGV[0] then numSecs = ARGV[0] end
-    puts "Sleeping for " + numSecs.to_s + " seconds"
-    sleep numSecs.to_i
-    puts "Done sleeping"
-  end
-
-end
+numSecs = 5 #default
+if ARGV[0] then numSecs = ARGV[0] end
+puts "Sleeping for " + numSecs.to_s + " seconds"
+sleep numSecs.to_i
+puts "Done sleeping"
 
